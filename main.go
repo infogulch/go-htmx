3bbaba9d3d79081f90198100e8f610b0a9c9db3d
Joe Taber / infogulch <infogulch@gmail.com>
2023-01-21 13:12:16 -0600

    Minor cleanup, rename

--- main.go
+++ main.go
@@ -61,11 +61,20 @@ CREATE TABLE kv (
 	key TEXT PRIMARY KEY NOT NULL,
 	value ANY NOT NULL
 ) STRICT, WITHOUT ROWID;
+
 CREATE TABLE todos (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	done INTEGER NOT NULL CHECK (done BETWEEN 0 AND 1),
 	label TEXT NOT NULL
 ) STRICT;
+
+CREATE VIEW todos_filtered AS
+WITH f(filter) AS (SELECT value FROM kv WHERE key = 'todos_filter')
+SELECT id, done, label
+FROM todos
+JOIN f
+WHERE done AND f.filter IN ('all','completed')
+OR NOT done AND f.filter IN ('all','active');
 `}
 
 func setupDB(db *sql.DB) {
@@ -97,7 +106,7 @@ func index(db *sql.DB, fs TemplateFS) http.HandlerFunc {
 			log.Printf("failed to initialize kv %s = 0: %v", KEY, err)
 		}
 	}
-	return hx_template_handler(fs, files, template.FuncMap{
+	return TemplateHandler(fs, files, template.FuncMap{
 		"counter": func() (counter int, err error) {
 			err = db.QueryRow("SELECT value FROM kv WHERE key = ?;", KEY).Scan(&counter)
 			return
@@ -134,14 +143,15 @@ func todos(db *sql.DB, fs TemplateFS) http.HandlerFunc {
 	}
 
 	files := []string{"layout.html", "todos.html"}
-	return hx_template_handler(fs, files, template.FuncMap{
+	return TemplateHandler(fs, files, template.FuncMap{
 		"new": func(data url.Values) (todo Todo, err error) {
 			todo.Label = data.Get("newtodo")
 			if todo.Label == "" {
 				err = fmt.Errorf("empty todo")
 				return
 			}
-			res, err := db.Exec("INSERT INTO todos(done, label) VALUES (false, ?);", &todo.Label)
+			var res sql.Result
+			res, err = db.Exec("INSERT INTO todos(done, label) VALUES (false, ?);", &todo.Label)
 			if err != nil {
 				return
 			}
@@ -149,14 +159,16 @@ func todos(db *sql.DB, fs TemplateFS) http.HandlerFunc {
 			return
 		},
 		"toggleall": func() (changed bool, err error) {
-			res, err := db.Exec("UPDATE todos SET done = NOT (SELECT COALESCE(MIN(done),0) from todo)")
+			var res sql.Result
+			res, err = db.Exec("UPDATE todos SET done = NOT (SELECT COALESCE(MIN(done),0) from todos)")
 			if affected, _ := res.RowsAffected(); affected > 0 {
 				changed = true
 			}
 			return
 		},
 		"toggle": func(id string) (todo Todo, err error) {
-			idn, err := strconv.ParseInt(id, 10, 64)
+			var idn int64
+			idn, err = strconv.ParseInt(id, 10, 64)
 			if err != nil {
 				return
 			}
@@ -171,19 +183,21 @@ func todos(db *sql.DB, fs TemplateFS) http.HandlerFunc {
 			err = db.QueryRow("SELECT id, done, label FROM todos WHERE id = ?", idn).Scan(scanTodo(&todo)...)
 			return
 		},
-		"delete": func(id string) (Unit, error) {
-			idn, err := strconv.ParseInt(id, 10, 64)
+		"delete": func(id string) (_ struct{}, err error) {
+			var idn int64
+			idn, err = strconv.ParseInt(id, 10, 64)
 			if err != nil {
-				return Unit{}, err
+				return
 			}
-			res, err := db.Exec("DELETE FROM todos WHERE id = ?", idn)
+			var res sql.Result
+			res, err = db.Exec("DELETE FROM todos WHERE id = ?", idn)
 			if affected, _ := res.RowsAffected(); affected != 1 {
-				return Unit{}, fmt.Errorf("invalid todo id")
+				return struct{}{}, fmt.Errorf("invalid todo id")
 			}
 			if err != nil {
-				return Unit{}, err
+				return
 			}
-			return Unit{}, nil
+			return
 		},
 		"alldone": func() (done bool, err error) {
 			err = db.QueryRow("SELECT COUNT(1) > 0 AND COUNT(1) = SUM(done) FROM todos").Scan(&done)
@@ -198,7 +212,8 @@ func todos(db *sql.DB, fs TemplateFS) http.HandlerFunc {
 				err = fmt.Errorf("invalid filter")
 				return
 			}
-			res, err := db.Exec("UPDATE kv SET value = ? where key = ?", filter, FILTER_KEY)
+			var res sql.Result
+			res, err = db.Exec("UPDATE kv SET value = ? where key = ?", filter, FILTER_KEY)
 			if err != nil {
 				return
 			}
@@ -208,14 +223,7 @@ func todos(db *sql.DB, fs TemplateFS) http.HandlerFunc {
 			return
 		},
 		"todos": func() ([]Todo, error) {
-			return RowsScanner(scanTodo)(db.Query(
-				`WITH f(filter) AS (SELECT value FROM kv WHERE key = ?)
-				SELECT id, done, label
-				FROM todos
-				JOIN f
-				WHERE done = 1 AND f.filter in ('all','completed')
-				OR done = 0 AND f.filter in ('all','active')
-				ORDER BY id DESC;`, FILTER_KEY))
+			return RowsScanner(scanTodo)(db.Query(`SELECT id, done, label FROM todos_filtered`))
 		},
 		"todo": func(id string) (todo Todo, err error) {
 			err = db.QueryRow("SELECT id, done, label FROM todos WHERE id = ?", id).Scan(scanTodo(&todo)...)
@@ -245,42 +253,52 @@ func RowsScanner[R any](getDest func(*R) []interface{}) func(*sql.Rows, error) (
 	}
 }
 
-type Unit struct{}
-
-func UnitFn() Unit { return Unit{} }
-
-func hx_template_handler(fs TemplateFS, files []string, funcs template.FuncMap) http.HandlerFunc {
+// TemplateHandler constructs an html.Template from the provided args
+// and returns an http.HandlerFunc that routes requests to named nested
+// template definitions based on a routeId derived from URL query params.
+//
+// routeId is a string constructed from the request:
+// - If the request has header HX-Request=true, then it's prefixed with `hx-`
+// - The http method
+// - A sorted list of unique URL query params
+//
+// These parts are joined with '-' and lowercased, then used to look up
+// a template definition; if found, the template is rendered as a response,
+// else it renders a 404.
+//
+// Example requests and matching routeId:
+// - Plain HTTP GET: get
+// - HTTP GET with HX-Request header: hx-get
+// - HTTP POST with nav param: post-nav
+// - HTTP DELETE with HX-Request header and id param: hx-delete-id
+// - HTTP POST with tYPe and iD params: post-id-type
+func TemplateHandler(fs TemplateFS, files []string, funcs template.FuncMap) http.HandlerFunc {
 	tmpl, err := template.New(files[0]).Funcs(funcs).ParseFS(fs, files...)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-		params := r.Form
-
-		// Construct the routeId, re ~= (hx-)?{httpMethod}{-urlParams}
-		// Examples: get, hx-get, post-id, hx-post-id
-
-		keys := maps.Keys(params)
-		sort.Strings(keys)
-		routeparts := append([]string{"hx", strings.ToLower(r.Method)}, keys...)
-		if r.Header.Get("HX-Request") != "true" {
-			routeparts = routeparts[1:]
+		var routeId string
+		{
+			keys := maps.Keys(r.URL.Query())
+			sort.Strings(keys)
+			routeparts := append([]string{"hx", r.Method}, keys...)
+			if r.Header.Get("HX-Request") != "true" {
+				routeparts = routeparts[1:]
+			}
+			routeId = strings.ToLower(strings.Join(routeparts, "-"))
 		}
-		routeId := strings.Join(routeparts, "-")
 
-		log.Printf("Executing route: %s : %s", r.URL.Path, routeId)
+		log.Printf("Handling route at %s : %s", r.URL.Path, routeId)
 
-		var err error
-		if routeId == "get" {
-			err = tmpl.Execute(w, params)
-		} else if t := tmpl.Lookup(routeId); t != nil {
-			err = t.Execute(w, params)
+		if t := tmpl.Lookup(routeId); t != nil {
+			r.ParseForm()
+			err := t.Execute(w, r.Form)
+			if err != nil {
+				log.Print(err)
+			}
 		} else {
 			http.NotFound(w, r)
-		}
-		if err != nil {
-			log.Print(err)
 		}
 	}
 }
